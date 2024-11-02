e3aab5606ca2bbd1d094e0de918db0bd06d21b5b
nadiaresti_ <nadiarpermatasari@gmail.com>
2024-11-02 19:21:44 +0700

    Fix pagination

--- resources/views/vendor/pagination/custom.blade.php
+++ resources/views/vendor/pagination/custom.blade.php
@@ -8,16 +8,35 @@
 
 @if ($paginator->hasPages())
 <ul class="pagination pagination-sm m-0 mt-3 float-right">
+    {{-- Previous Page Link --}}
     @if ($paginator->onFirstPage())
         <li class="page-item"><a class="page-link disabled" href="#">&laquo;</a></li>
     @else
-    <li class="page-item"><a class="page-link" href="#">&laquo;</a></li>
+        <li class="page-item"><a class="page-link" href="{{ $paginator->previousPageUrl() }}">&laquo;</a></li>
     @endif
 
+    {{-- Pagination Elements --}}
+    @foreach ($elements as $each)
+        @if (is_string($each))
+            <li class="page-item disabled"><span>{{ $each }}</span></li>
+        @endif
 
-    <li class="page-item"><a class="page-link" href="#">1</a></li>
-    <li class="page-item"><a class="page-link" href="#">2</a></li>
-    <li class="page-item"><a class="page-link" href="#">3</a></li>
-    <li class="page-item"><a class="page-link" href="#">&raquo;</a></li>
+        @if (is_array($each))
+            @foreach ($each as $page => $url)
+                @if ($page == $paginator->currentPage())
+                    <li class="page-item active"><a class="page-link disabled">{{ $page }}</a></li>
+                @else
+                    <li class="page-item"><a class="page-link" href="{{ $url }}">{{ $page }}</a></li>
+                @endif
+            @endforeach
+        @endif
+    @endforeach
+
+    {{-- Next Page Link --}}
+    @if ($paginator->hasMorePages())
+        <li class="page-item"><a class="page-link" href="{{ $paginator->nextPageUrl() }}">&raquo;</a></li>
+    @else
+        <li class="page-item"><a class="page-link disabled" href="#">&raquo;</a></li>
+    @endif
 </ul>
 @endif
