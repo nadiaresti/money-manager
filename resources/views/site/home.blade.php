99842c801468005e5c2ab27a617605233f8c3099
nadiaresti_ <nadiarpermatasari@gmail.com>
2024-11-13 22:44:07 +0700

    Home: create chart in dashboard

--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -1,4 +1,6 @@
 <?php
+use App\Models\Transaction;
+use App\Helpers\GeneralHelper;
 $title = 'Welcome';
 $breadcrumbs = [
   ['url' => '#', 'label' => 'Home'],
@@ -10,42 +12,168 @@ $breadcrumbs = [
 <div class="container-fluid">
   <!-- Small boxes (Stat box) -->
   <div class="row">
-    <div class="col-lg-3 col-6">
-      <!-- small box -->
-      <div class="small-box bg-info">
-        <div class="inner">
-          <h3>150</h3>
-
-          <p>Income</p>
+    <!-- Info Box (Income) -->
+    <div class="col-12 col-sm-6 col-md-3">
+      <div class="info-box">
+        <span class="info-box-icon bg-success elevation-1"><i class="fas fa-money-bill-wave"></i></span>
+        <div class="info-box-content">
+          <span class="info-box-text">Income</span>
+          <span class="info-box-number">{{ GeneralHelper::formatMoney($total_income) }}</span>
         </div>
-        <div class="icon">
-          <i class="ion ion-bag"></i>
-        </div>
-        <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
       </div>
     </div>
-    <!-- ./col -->
-    <div class="col-lg-3 col-6">
-      <!-- small box -->
-      <div class="small-box bg-success">
-        <div class="inner">
-          <h3>53<sup style="font-size: 20px">%</sup></h3>
+    <!-- End of Info box -->
 
-          <p>Expense</p>
+    <!-- Info box (Expense) -->
+    <div class="col-12 col-sm-6 col-md-3">
+      <div class="info-box">
+        <span class="info-box-icon bg-danger elevation-1"><i class="fas fa-dollar-sign"></i></span>
+        <div class="info-box-content">
+          <span class="info-box-text">Expense</span>
+          <span class="info-box-number">{{ GeneralHelper::formatMoney($total_expense) }}</span>
         </div>
-        <div class="icon">
-          <i class="ion ion-stats-bars"></i>
-        </div>
-        <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
       </div>
     </div>
-    <!-- ./col -->
+    <!-- End of Info box -->
   </div>
-  <!-- /.row -->
+  <!-- End of Small boxes (Stat box) -->
+
   <!-- Main row -->
   <div class="row">
+    <!-- Pie Chart (Income) -->
+    <div class="col-lg-6">
+      <div class="card">
+          <div class="card-body">
+            <p class="d-flex flex-column">
+              <?php
+                if ($income_diff > 0) $icon = 'fa-arrow-up';
+                elseif ($income_diff < 0) $icon = 'fa-arrow-down';
+                else $icon = 'fa-circle';
 
+                if ($income_diff > 0) $text_color = 'text-success';
+                elseif ($income_diff < 0) $text_color = 'text-danger';
+                else $text_color = 'text-secondary'; ?>
+
+              <span class="{{ $text_color }}">
+                <i class="fas {{ $icon }}"></i>
+                {{ GeneralHelper::formatMoney($income_diff) }}
+              </span>
+              <span>Income Detail</span>
+            </p>
+              <div class="chartjs-size-monitor">
+              <div class="chartjs-size-monitor-expand">
+                <div class=""></div>
+              </div>
+              <div class="chartjs-size-monitor-shrink">
+                <div class=""></div></div>
+              </div>
+              @if (!empty($income_pie))
+                <canvas id="pie-chart-income" style="min-height: 250px; height: 250px; max-height: 250px; max-width: 100%; display: block; width: 535px;" width="936" height="437" class="chartjs-render-monitor"></canvas>
+              @else
+                <div style="min-height: 250px; height: 250px; max-height: 250px; max-width: 100%; display: flex; justify-content: center; align-items: center; text-align: center;">
+                  <i class="text-secondary">Data Not Available</i>
+                </div>
+              @endif
+          </div>
+      </div>
+    </div>
+    <!-- End of pie chart (income) -->
+
+    <!-- Pie Chart (Expense) -->
+    <div class="col-lg-6">
+      <div class="card">
+          <div class="card-body">
+            <p class="d-flex flex-column">
+              <?php
+                if ($expense_diff > 0) $icon = 'fa-arrow-up';
+                elseif ($expense_diff < 0) $icon = 'fa-arrow-down';
+                else $icon = 'fa-circle';
+
+                if ($expense_diff > 0) $text_color = 'text-danger';
+                elseif ($expense_diff < 0) $text_color = 'text-success';
+                else $text_color = 'text-secondary';
+                ?>
+
+              <span class="{{ $text_color }}">
+                <i class="fas {{ $icon }}"></i>
+                {{ GeneralHelper::formatMoney($expense_diff) }}
+              </span>
+              <span>Expense Detail</span>
+            </p>
+              <div class="chartjs-size-monitor">
+              <div class="chartjs-size-monitor-expand">
+                <div class=""></div>
+              </div>
+              <div class="chartjs-size-monitor-shrink">
+                <div class=""></div></div>
+              </div>
+              @if (!empty($expense_pie))
+                <canvas id="pie-chart-expense" style="min-height: 250px; height: 250px; max-height: 250px; max-width: 100%; display: block; width: 535px;" width="936" height="437" class="chartjs-render-monitor"></canvas>
+              @else
+                <div style="min-height: 250px; height: 250px; max-height: 250px; max-width: 100%; display: flex; justify-content: center; align-items: center; text-align: center;">
+                  <i class="text-secondary">Data Not Available</i>
+                </div>
+              @endif
+          </div>
+      </div>
+    </div>
+    <!-- End of pie chart (expense) -->
   </div>
   <!-- /.row (main row) -->
 </div><!-- /.container-fluid -->
+@endsection
+
+@section('scripts')
+<script>
+  // Generate random colors for the chart
+  const backgroundColors = [];
+  const borderColors = [];
+  const numColors = @json($income_label).length > @json($expense_label).length ? @json($income_label).length : @json($expense_label).length;
+  for (let i = 0; i < numColors; i++) {
+    backgroundColors.push(getRandomColor());
+    borderColors.push(getRandomColor());
+  }
+
+  const pieChartIncome = document.getElementById('pie-chart-income');
+  if (pieChartIncome) {
+    pieChartIncome.getContext('2d');
+    const setPieChart = new Chart(pieChartIncome, {
+      type: 'pie',
+      data: {
+          labels: @json($income_label),
+          datasets: [{
+              label: 'Income',
+              data: @json($income_pie),
+              backgroundColor: @json($income_label).map(() => getRandomColor()),
+              borderColor: borderColors,
+              borderWidth: 1
+          }]
+      },
+    });
+  }
+
+  const pieChartExpense = document.getElementById('pie-chart-expense');
+  if (pieChartExpense) {
+    pieChartExpense.getContext('2d');
+    const setPieChartExpense = new Chart(pieChartExpense, {
+      type: 'pie',
+      data: {
+        labels: @json($expense_label),
+        datasets: [{
+          label: 'Expense',
+          data: @json($expense_pie),
+          backgroundColor: backgroundColors,
+          borderColor: borderColors,
+          borderWidth: 1,
+        }]
+      }
+    })
+  }
+
+  function getRandomColor() {
+    const randomColor = Math.floor(Math.random()*16777215).toString(16);
+    return `#${randomColor.padStart(6, '0')}`;
+  }
+</script>
+
 @endsection
