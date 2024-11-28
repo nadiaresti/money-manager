d5b864c8ddf8e6860d8c1bbc8108e2ce5de289a4
nadiaresti_ <nadiarpermatasari@gmail.com>
2024-11-28 14:22:21 +0700

    General Helper JS: fix trigger of format-number class

--- public/js/general-helper.js
+++ public/js/general-helper.js
@@ -1,16 +1,21 @@
 // General Helper
+$(document).ready(function() {
+  $('.format-number').each(function() {
+    formatNumber($(this));
+  });
 
-$(document).on('keyup', '.format-number', function(){
-  let num = $(this).val();
+  $(document).on('keyup', '.format-number', function(){
+    formatNumber($(this));
+  })
+})
+
+function formatNumber(element) {
+  let num = element.val();
   // remove comma
   num = num.replace(/,/g, '');
   // format number
   let temp = num.toString().split('.');
   temp[0] = temp[0].replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 
-  $(this).val(temp.join('.'));
-})
-
-
-
-
+  element.val(temp.join('.'));
+}
